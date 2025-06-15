bb42e78ea252791ff08f82721fee69d24c738e19
Valerii Odintsov <skif1998@gmail.com>
2025-06-15 13:06:26 +0200

    Task 1

---
+++ task_1.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def get_days_from_today(date: str) -> int | None:
+    try:
+        input_date = datetime.strptime(date, "%Y-%m-%d").date()
+        date_today = datetime.today().date()
+        days_difference = (date_today - input_date).days
+        return days_difference
+    except ValueError:
+        print("Invalid date format. Please use YYYY-MM-DD.")
+        return None
